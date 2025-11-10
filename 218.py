52aea72fa1c02dd5dcc757bf132eeeb9ca0f27e8
samarawwleticia <samaradddsantos@gmail.com>
2025-11-10 19:04:07 -0300

    escolhi o 1o problema e vou implementar diviidr e conquistar

---
+++ 218.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+
+        return self._resolver_recursivamente(buildings)
+
+    def _resolver_recursivamente(self, predios_atuais: List[List[int]]) -> List[List[int]]:
+        
+        #func alg dividir recursiva
+
+        pass
+
+    def _mesclar_skylines(self, esq: List[List[int]], dir: List[List[int]]) -> List[List[int]]:
+        
+        #conquistar
+
+        pass
